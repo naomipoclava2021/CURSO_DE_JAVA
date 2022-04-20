e62e91992c10b71e392a6ea5c0599825a43a2806
naomipoclava2021 <milagrito1dediciembre@gmail.com>
2022-04-19 23:40:57 -0300

    Ordenamiento por inserccion

--- OrdenamientoJava/src/bloque6/MetodoBurbuja.java
+++ OrdenamientoJava/src/bloque6/MetodoBurbuja.java
@@ -1,10 +1,60 @@
 package bloque6;
 
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
 public class MetodoBurbuja {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hola soy naomi");
+		/*
+		 * 				si
+		 * numeroActual > nuemeroSiguiente
+		 * 			cambio
+		 */
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		int arreglo[], nElementos, aux;
+		
+		nElementos= Integer.parseInt(JOptionPane.showInputDialog("Digite la cantidad de elemetos de arreglo: "));
+		
+		arreglo= new int[nElementos]; //Le asignamos el numero de elemento
+		
+		System.out.println("Llenar el arreglo");
+		
+		for (int i =0; i<nElementos; i++) {
+			System.out.print((i+1)+". Digite un numero: ");
+			arreglo[i]= entrada.nextInt();
+		}
+		
+		//Metodo burbuja
+		for (int i=0; i<nElementos-1; i++) {
+			for (int j=0; j<nElementos-1; j++) {
+				if (arreglo[j]> arreglo[j+1]) {//Si numeroActual > numeroSiguiente
+					aux = arreglo[j];
+					arreglo[j]=arreglo[j+1];
+					arreglo[j+1]= aux;
+				}
+			}
+		}
+		
+		//Mostrar el arreglo ordenado en forma creciente
+		System.out.print("\nArreglo oredanado en forma creciente: ");
+		
+		for (int i=0; i<nElementos; i++) {
+			System.out.print(arreglo[i]+ " - ");
+		}
+		System.out.println();
+		
+		//Mostat el arreglo ordenado en forma decrecite
+		System.out.print("\nArreglo oredanado en forma decreciente: ");
+		for (int i= (nElementos-1); i>=0; i--) {
+			System.out.print(arreglo[i]+ " - ");
+		}
+		System.out.println();
+		entrada.close();
 	}
 
 }
---
+++ OrdenamientoJava/src/bloque6/OrdenamientoPorInserccion.java
@@ -0,0 +1,54 @@
+package bloque6;
+
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class OrdenamientoPorInserccion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * 			si
+		 * numuroIzq > numeroActual
+		 * 			camhio
+		 */
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		int arreglo[], nElemento, pos, aux;
+		nElemento= Integer.parseInt(JOptionPane.showInputDialog("Digite el numero de elementos"));
+		arreglo = new int[nElemento];
+		
+		System.out.println("Rellene el arreglo");
+		for (int i =0; i<nElemento; i++) {
+			System.out.print((i+1)+ " Digite un numero: ");
+			arreglo[i]= entrada.nextInt();
+		}
+		
+		//Ordenamiento por Inserccion
+		
+		for (int i =0; i<nElemento; i++) {
+			pos = i;
+			aux = arreglo[i];
+			
+			while((pos>0) && (arreglo[pos-1]> aux)) {
+				arreglo[pos]= arreglo[pos-1];
+				pos--;
+			}
+			arreglo[pos] = aux;
+		}
+		System.out.print("\nOrden Ascendente: ");
+		for (int i=0; i<nElemento; i++) {
+			System.out.print(arreglo[i]+ " - ");
+		}
+		System.out.println("");
+		
+		System.out.print("\nOrden Decendente: ");
+		for (int i = (nElemento-1); i>=0; i--) {
+			System.out.print(arreglo[i]+ " - ");
+		}
+		System.out.println("");
+	}
+
+}
